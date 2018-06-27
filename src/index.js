d87b6bba32262e6270a4263490c1059a20955402
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2018-06-27 17:30:40 -0400

    Added Routing as per SA5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 // import $ from 'jquery';
 import './style.scss';
 // const $ = require('jquery');
 
+const About = (props) => {
+  return <div>All there is to know about me</div>;
+};
+
+const Welcome = (props) => {
+  return (
+    <div>Welcome</div>
+  );
+};
+
+const Test = (props) => {
+  return <div>ID: {props.match.params.id}</div>;
+};
+
+const FallBack = (props) => {
+  return <div> URL Not Found </div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
 const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('main'));
